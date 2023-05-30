a3bd6081ce2ee5061dfe45a6b1cc0abbd345c935
201122129 <estebandls95@gmail.com>
2023-05-30 14:00:11 -0700

    sasdsdas

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -7,31 +7,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnBass1;
-
-    private Button btnBass2;
-
-    private Button btnKicker;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-
-        btnBass1 = findViewById(R.id.btn_Bass1);
-        btnBass2 = findViewById(R.id.btn_Bass2);
-        btnKicker = findViewById(R.id.btn_Kicker;
-
-
-        btnBass1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                playBass1;
-            }
-        });
-    }
+    private button btnBass1
 
 
 }
